5bc61465d9c75d132682599e699337bdc3784b4a
rubyazz <ersultan.abduvalov@gmail.com>
2022-12-22 08:46:24 +0600

    resting

--- program/utils.py
+++ program/utils.py
@@ -2,33 +2,28 @@ from datetime import datetime, timedelta, date
 from calendar import HTMLCalendar
 from .models import Event
 
+
 class Calendar(HTMLCalendar):
 	def __init__(self, year=None, month=None):
 		self.year = year
 		self.month = month
 		super(Calendar, self).__init__()
 
-	# formats a day as a td
-	# filter events by day
 	def formatday(self, day, events):
 		events_per_day = events.filter(start_time__day=day)
 		d = ''
 		for event in events_per_day:
 			d += f'<li> {event.get_html_url} </li>'
-
 		if day != 0:
 			return f"<td><span class='date'>{day}</span><ul> {d} </ul></td>"
 		return '<td></td>'
 
-	# formats a week as a tr
 	def formatweek(self, theweek, events):
 		week = ''
 		for d, weekday in theweek:
 			week += self.formatday(d, events)
 		return f'<tr> {week} </tr>'
 
-	# formats a month as a table
-	# filter events by year and month
 	def formatmonth(self, withyear=True):
 		events = Event.objects.filter(start_time__year=self.year, start_time__month=self.month)
 
